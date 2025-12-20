7f40487ced9d646a3dc94d8ecc2f44f060d37a89
Melih AK <melihak2467@gmail.com>
2025-12-20 17:00:11 +0300

    niri dotfiles added

---
+++ niri/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1. Önce Vivaldi açılsın (Böylece "browser" workspace'i ilk sıraya yerleşir)
+#vivaldi &
+#sleep 2 # Niri'nin workspace'i oluşturması için kısa bir bekleme
+
+# 2. Sonra VSCode
+#code &
+#sleep 2
+
+# 3. En son Obsidian
+#obsidian &
