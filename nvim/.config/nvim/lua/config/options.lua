5c8842e2a7fe007c2fbcb343fba1b702c8c4d0c5
Melih AK <melihak2467@gmail.com>
2026-02-05 15:54:20 +0300

    fix(nvim): align clipboard provider with disabled primary selection in plasma

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,17 @@
 -- Add any additional options here
 
 vim.opt.spelllang = { "en", "tr" }
+
+-- nvim (LazyVim yapısında)
+vim.g.clipboard = {
+  name = "wl-copy",
+  copy = {
+    ["+"] = "wl-copy",
+    ["*"] = "wl-copy", -- '--primary' flag'ini sildik
+  },
+  paste = {
+    ["+"] = "wl-paste",
+    ["*"] = "wl-paste", -- '--primary' flag'ini sildik
+  },
+  cache_enabled = 1,
+}
